22512b957ea6493e253d076b6e8cce5020943648
Imron2704 <pythonwithimron@gmail.com>
2022-11-28 13:21:17 +0500

    dsff

--- apiv0/serializer.py
+++ apiv0/serializer.py
@@ -2,19 +2,19 @@ from main.models import *
 from rest_framework import serializers
 
 
-class TeacherSerializer(serializers.ModelSerializer):
+class TypeSerializer(serializers.ModelSerializer):
     class Meta:
         model = Types_of_Freight
         fields = '__all__'
 
 
-class StudentSerializer(serializers.ModelSerializer):
+class CostSerializer(serializers.ModelSerializer):
     class Meta:
         model = Calculate_the_Cost
         fields = '__all__'
 
 
-class SubjectSerializer(serializers.ModelSerializer):
+class SubmitSerializer(serializers.ModelSerializer):
     class Meta:
         model = Submit_your_application
         fields = '__all__'
