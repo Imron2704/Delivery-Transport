1b2457eb05e73d620eced997ebc8659cd29ac062
Imron2704 <pythonwithimron@gmail.com>
2022-11-28 18:52:04 +0500

    changes

--- main/models.py
+++ main/models.py
@@ -51,12 +51,6 @@ class Submit_your_application():
         return self.stuff
 
 
-class About_Company():
-    title = models.CharField(max_length = 150)
-    description = models.TextField()
-    image = models.ImageField(upload_to = 'image/about_pictures', null = True)
-
-
 class User():
     email = models.EmailField()
     password = models.CharField(max_length = 100)
@@ -69,33 +63,10 @@ class User():
         return self.email
 
 
-class Order(models.Model):
-    # user = models.ForeignKey(Account, on_delete=models.CASCADE)
-    # car = models.ForeignKey(Car, on_delete=models.CASCADE)
-    title = models.CharField(max_length=65)
-    date_created = models.DateTimeField(auto_now_add=True, blank=True)
-    from_here = models.CharField(max_length=50)
-    to_here = models.CharField(max_length=50)
-    phone_number = models.IntegerField()
-    # weight_cargo = models.ForeignKey(WeightCargo, on_delete=models.CASCADE)
-    # volume_cargo = models.ForeignKey(VolumeCargo, on_delete = models.CASCADE)
-    # type_cargo = models.ForeignKey(TypeCargo, on_delete=models.CASCADE)
-    # mode_cargo = models.ForeignKey(ModeCargo, on_delete=models.CASCADE)
-    image = models.ImageField(upload_to='apps/order/', null=True)
-    image2 = models.ImageField(upload_to='apps/order/', null=True, blank=True)
-    image3 = models.ImageField(upload_to='apps/order/', null=True, blank=True)
-
-    def __str__(self) -> str:
-        return self.title, self.image
-
-
-
 # class Services():
 #     pass
 
 
-# class Blog():
-#     pass
 
 
 # class For_business():
@@ -103,8 +74,4 @@ class Order(models.Model):
 
 
 # class Individuals():
-#     pass
-
-
-# class Contacts():
 #     pass
