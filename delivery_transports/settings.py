dfdc73c4c2bc23feec83b8205f6adc9e49655297
Imron2704 <pythonwithimron@gmail.com>
2022-11-30 15:10:22 +0500

    archives

--- delivery_transports/settings.py
+++ delivery_transports/settings.py
@@ -72,14 +72,6 @@ REST_FRAMEWORK = {
     )
 }
 
-# REST_FRAMEWORK = {
-#     'DEFAULT_AUTHENTICATION_CLASSES': [
-#         # 'rest_framework.authentication.BasicAuthentication',
-#         # 'rest_framework.authentication.SessionAuthentication',
-#         'knox.auth.TokenAuthentication',
-#     ]
-# }
-
 ROOT_URLCONF = 'delivery_transports.urls'
 
 TEMPLATES = [
