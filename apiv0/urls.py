99ef8f13fd6e35832df076de1e5755187fd4e22c
Imron2704 <pythonwithimron@gmail.com>
2022-11-30 15:38:36 +0500

    dgrdf

--- apiv0/urls.py
+++ apiv0/urls.py
@@ -7,4 +7,25 @@ urlpatterns = [
     path('order/', RegisterAPI.as_view(), name='trucks'),
     path('blog/', RegisterAPI.as_view(), name='trucks'),
     path('calculate/', RegisterAPI.as_view(), name='trucks'),
+
+    ##### Trucks/apiv0 #####
+    # path('teachers/', TeacherViewSet.as_view({'get': 'list'}), name = 'teachers'),
+    # path('teachers/create', TeacherCreateView.as_view()),
+    # path('teachers/change', TeacherUpdateView.as_view()),
+    # path('teachers/delete', TeacherDestroyView.as_view()),
+    # ##### Students/apiv0 #####
+    # path('students/', StudentViewSet.as_view({'get': 'list'}), name = 'students'),
+    # path('students/create', StudentCreateView.as_view()),
+    # path('students/change', StudentUpdateView.as_view()),
+    # path('students/delete', StudentDestroyView.as_view()),
+    # ##### Subjects/apiv0 #####
+    # path('subjects/', SubjectViewSet.as_view({'get': 'list'}), name = 'subjects'),
+    # path('subjects/create', SubjectCreateView.as_view()),
+    # path('subjects/change', SubjectUpdateView.as_view()),
+    # path('subjects/delete', SubjectDestroyView.as_view()),
+    
+    # ##### Api Token SimpleJWT #####
+    # path('api/token/', TokenObtainPairView.as_view(), name = 'token_obtain_pair'),
+    # path('api/token/refresh/', TokenRefreshView.as_view(), name = 'token_refresh'),
+    # path('api/token/verify/', TokenVerifyView.as_view(), name = 'token_verify'),
 ]
--- apiv0/views.py
+++ apiv0/views.py
@@ -16,3 +16,35 @@ class RegisterAPI(generics.GenericAPIView):
         "user": UserSerializer(user, context=self.get_serializer_context()).data,
         "token": AuthToken.objects.create(user)[1]
         })
+
+
+# class TeacherViewSet(viewsets.ModelViewSet):
+#     serializer_class = TeacherSerializer
+#     permission_classes = [permissions.IsAuthenticated]
+
+#     def get_queryset(self):
+#         q = Teacher.objects.all()
+#         url_dict = self.request.GET
+#         print(url_dict)
+#         if 'search-text' in url_dict and url_dict['search-text']:
+#             text = url_dict.get('search-text')
+#             q = q.filter(Q(name__icontains=text))
+#         return q
+
+
+# class TeacherCreateView(generics.CreateAPIView):
+#     serializer_class = TeacherSerializer
+
+
+# class TeacherUpdateView(generics.UpdateAPIView):
+#     serializer_class = TeacherSerializer
+
+#     def get_object(self):
+#         return Teacher.objects.get(pk = self.request.data.get('id'))
+
+
+# class TeacherDestroyView(generics.DestroyAPIView):
+#     serializer_class = TeacherSerializer
+
+#     def get_object(self):
+#         return Teacher.objects.get(pk = self.request.data.get('id'))
